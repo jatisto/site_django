57b4a896bc7b677c2a5a7ca2b19ccde0dd84c837
jatisto <j.atisto@gmail.com>
2016-09-17 22:23:42 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# site_django
+# site_django
---
+++ django-john-env/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/eightfivenine/MikeHubertDjangoLesson/django-john-env/bin/python3
+# EASY-INSTALL-SCRIPT: 'Django==1.10.1','django-admin.py'
+__requires__ = 'Django==1.10.1'
+__import__('pkg_resources').run_script('Django==1.10.1', 'django-admin.py')
